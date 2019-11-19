ac6d6ea71b9ce524f9c9ffa328cbc9eed7444f90
Justin Wyatt <justinjwyatt@gmail.com>
2019-11-18 20:28:56 -0600

    Initial Commit

---
+++ Security Vault/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Security Vault
+//
+//  Created by Justin Wyatt on 11/18/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
